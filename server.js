54dc952dc4cf4aa630d5f96495033eda897bf03d
ebugsky <ebugsky@gmail.com>
2011-10-25 22:21:06 +0800

    Change Port to 8100.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('hello, nodejs!')
   res.end();
-}).listen(8000);
+}).listen(8100);
 
 
-console.log('> http server has started on port 8000');
+console.log('> http server has started on port 8100');
